efd4f9e34ed9a6a83b06027c3d28f0045437e16d
JGCA24 <96082939+JGCA24@users.noreply.github.com>
2024-07-15 00:39:05 -0400

    create folder data, models and views to local sqlite

---
+++ SmartHome/SmartHome/Views/UserDetailsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SmartHome.Models;
+using System;
+
+namespace SmartHome.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserDetailsPage : ContentPage
+    {
+        public UserDetailsPage(User user)
+        {
+            InitializeComponent();
+            DisplayUserData(user);
+        }
+
+        private void DisplayUserData(User user)
+        {
+            FirstNameLabel.Text = user.FirstName;
+            LastNameLabel.Text = user.LastName;
+            EmailLabel.Text = user.Email;
+        }
+
+        private async void OnCloseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
