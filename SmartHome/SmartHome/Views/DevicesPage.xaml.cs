3331ba18464931db894ac5c8b35ed1bcbe64df69
JGCA24 <96082939+JGCA24@users.noreply.github.com>
2024-08-04 17:46:19 -0400

    Nuevos Dispositivos Funcionales en mis Dispositivo

--- SmartHome/SmartHome/Views/DevicesPage.xaml.cs
+++ SmartHome/SmartHome/Views/DevicesPage.xaml.cs
@@ -8,6 +8,11 @@ namespace SmartHome.Views
     public partial class DevicesPage : ContentPage
     {
         private bool isLightOn = false;
+        private bool isVehicleOn = false;
+        private bool isAirConditionerOn = false;
+        private bool isCameraOn = false;
+        private bool isVacuumOn = false;
+        private bool isWaterPumpOn = false;
         private readonly HttpClient _httpClient;
 
         public DevicesPage()
@@ -25,22 +30,121 @@ namespace SmartHome.Views
                 ToggleSwitch.Text = "On";
                 ToggleSwitch.BackgroundColor = Color.Green;
                 LightbulbImage.Source = "On.png";
-                await SendCommandToDevice("Power%20On");
+                await SendCommandToDevice("192.168.11.38", "Power%20On");
             }
             else
             {
                 ToggleSwitch.Text = "Off";
                 ToggleSwitch.BackgroundColor = Color.Red;
                 LightbulbImage.Source = "OFF.png";
-                await SendCommandToDevice("Power%20Off");
+                await SendCommandToDevice("192.168.11.38", "Power%20Off");
             }
         }
 
-        private async Task SendCommandToDevice(string command)
+        private async void OnVehicleToggleClicked(object sender, EventArgs e)
+        {
+            isVehicleOn = !isVehicleOn;
+
+            if (isVehicleOn)
+            {
+                VehicleToggle.Text = "On";
+                VehicleToggle.BackgroundColor = Color.Green;
+                VehicleImage.Source = "VehicleOn.png"; // Supone que tienes una imagen para el estado "On"
+                await SendCommandToDevice("192.168.11.39", "Power%20On");
+            }
+            else
+            {
+                VehicleToggle.Text = "Off";
+                VehicleToggle.BackgroundColor = Color.Red;
+                VehicleImage.Source = "Vehicle.png";
+                await SendCommandToDevice("192.168.11.39", "Power%20Off");
+            }
+        }
+
+        private async void OnAirConditionerToggleClicked(object sender, EventArgs e)
+        {
+            isAirConditionerOn = !isAirConditionerOn;
+
+            if (isAirConditionerOn)
+            {
+                AirConditionerToggle.Text = "On";
+                AirConditionerToggle.BackgroundColor = Color.Green;
+                AirConditionerImage.Source = "AirConditionerOn.png";
+                await SendCommandToDevice("192.168.11.40", "Power%20On");
+            }
+            else
+            {
+                AirConditionerToggle.Text = "Off";
+                AirConditionerToggle.BackgroundColor = Color.Red;
+                AirConditionerImage.Source = "AirConditioner.png";
+                await SendCommandToDevice("192.168.11.40", "Power%20Off");
+            }
+        }
+
+        private async void OnCameraToggleClicked(object sender, EventArgs e)
+        {
+            isCameraOn = !isCameraOn;
+
+            if (isCameraOn)
+            {
+                CameraToggle.Text = "View";
+                CameraToggle.BackgroundColor = Color.Green;
+                CameraImage.Source = "CameraOn.png";
+                await SendCommandToDevice("192.168.11.41", "Power%20On");
+            }
+            else
+            {
+                CameraToggle.Text = "Photo";
+                CameraToggle.BackgroundColor = Color.Red;
+                CameraImage.Source = "Camera.png";
+                await SendCommandToDevice("192.168.11.41", "Power%20Off");
+            }
+        }
+
+        private async void OnVacuumToggleClicked(object sender, EventArgs e)
+        {
+            isVacuumOn = !isVacuumOn;
+
+            if (isVacuumOn)
+            {
+                VacuumToggle.Text = "On";
+                VacuumToggle.BackgroundColor = Color.Green;
+                VacuumImage.Source = "VacuumOn.png";
+                await SendCommandToDevice("192.168.11.42", "Power%20On");
+            }
+            else
+            {
+                VacuumToggle.Text = "Off";
+                VacuumToggle.BackgroundColor = Color.Red;
+                VacuumImage.Source = "Vacuum.png";
+                await SendCommandToDevice("192.168.11.42", "Power%20Off");
+            }
+        }
+
+        private async void OnWaterPumpToggleClicked(object sender, EventArgs e)
+        {
+            isWaterPumpOn = !isWaterPumpOn;
+
+            if (isWaterPumpOn)
+            {
+                WaterPumpToggle.Text = "On";
+                WaterPumpToggle.BackgroundColor = Color.Green;
+                WaterPumpImage.Source = "WaterPumpOn.png";
+                await SendCommandToDevice("192.168.11.43", "Power%20On");
+            }
+            else
+            {
+                WaterPumpToggle.Text = "Off";
+                WaterPumpToggle.BackgroundColor = Color.Red;
+                WaterPumpImage.Source = "WaterPump.png";
+                await SendCommandToDevice("192.168.11.43", "Power%20Off");
+            }
+        }
+
+        private async Task SendCommandToDevice(string ipAddress, string command)
         {
             try
             {
-                var ipAddress = "192.168.11.38"; 
                 var url = $"http://{ipAddress}/cm?cmnd={command}";
                 var response = await _httpClient.GetAsync(url);
 
